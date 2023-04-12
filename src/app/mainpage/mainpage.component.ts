3cd66f20010c6e72f4f3226b3d0b70f1fbfdddeb
Mike <theonlybuschi@gmail.com>
2023-04-12 14:41:14 +0200

    A

--- src/app/mainpage/mainpage.component.ts
+++ src/app/mainpage/mainpage.component.ts
@@ -6,29 +6,60 @@ import {HttpClient} from "@angular/common/http";
   templateUrl: 'mainpage.component.html',
   styleUrls: ['mainpage.component.css']
 })
-export class MainpageComponent{
+export class MainpageComponent {
 
-  constructor(private httpClient: HttpClient) {}
-
-  enteredValue = '';
-  germanText = '';
-  correctedText = '';
-  optimizedText = '';
-
-  onTranslateToDE(){
-    let textOB = {text: this.enteredValue};
-    this.httpClient.post<{text: string}>('http://localhost:5000/translate/DE', textOB).subscribe((response) => {
-      console.log(response.text);
-      this.germanText = response.text;
-    })
-    // this.englishText = this.enteredValue;
+  constructor(private httpClient: HttpClient) {
   }
-  onCorrectText(){
-    let textOB = {text: this.germanText};
-    this.httpClient.post<{message: string, text: string}>('http://localhost:5000/optimize', textOB).subscribe((response) => {
-      console.log(response.text);
-      console.log(response.message);
-      this.correctedText = response.text;
+
+  originalText = '';
+  germanText = '';
+  editedText = '';
+  englishText = '';
+  delta = -100;
+
+  onTranslateToDE() {
+    let textOB = {text: this.originalText};
+    this.httpClient.post<{ germanTranslation: string }>('http://localhost:5000/translate/DE', textOB).subscribe((response) => {
+      this.germanText = response.germanTranslation;
+    })
+  }
+  onTranslateToEN(){
+    let textOB = {text: this.editedText};
+    this.httpClient.post<{ englishTranslation: string }>('http://localhost:5000/translate/EN', textOB).subscribe((response) => {
+      this.englishText = response.englishTranslation;
+    })
+  }
+  onCorrectText() {
+    if (this.editedText.length > 0){
+      let textOB = {text: this.editedText};
+      this.httpClient.post<{ optimizedTextGS: string }>('http://localhost:5000/optimize/gs-correction', textOB).subscribe((response) => {
+        this.editedText = response.optimizedTextGS;
+      })
+    }else {
+      let textOB = {text: this.germanText};
+      this.httpClient.post<{ optimizedTextGS: string }>('http://localhost:5000/optimize/gs-correction', textOB).subscribe((response) => {
+        this.editedText = response.optimizedTextGS;
+      })
+    }
+  }
+  onParaphraseText() {
+    if (this.editedText.length > 0) {
+      let textOB = {text: this.editedText};
+      this.httpClient.post<{ optimizedTextPara: string }>('http://localhost:5000/optimize/paraphrasing', textOB).subscribe((response) => {
+        console.log(response.optimizedTextPara);
+        this.editedText = response.optimizedTextPara;
+      })
+    } else {
+      let textOB = {text: this.germanText};
+      this.httpClient.post<{ optimizedTextPara: string }>('http://localhost:5000/optimize/paraphrasing', textOB).subscribe((response) => {
+        this.editedText = response.optimizedTextPara;
+      })
+    }
+  }
+  onCheckSimilarity(){
+    this.httpClient.get<{delta: any}>('http://localhost:5000/similarity').subscribe((response) => {
+      this.delta = response.delta.similarity;
+      console.log(this.delta);
     })
   }
 }
